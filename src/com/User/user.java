9db75d852c39e49982db66461581175e0c30b894
Kiri <1936684518@qq.com>
2024-06-04 10:07:49 +0800

    done

---
+++ src/com/User/user.java
@@ -0,0 +1,42 @@
+package com.User;
+
+public class user implements Comparable<user>{
+    private String Username;
+    private String Password;
+    user(String username,String password)
+    {
+        this.Username=username;
+        this.Password=password;
+    }
+    public void setUsername(String username) {
+        Username = username;
+    }
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    @Override
+    public int compareTo(user o) {
+
+        return this.getUsername().compareTo(o.getUsername());
+    }
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+    public boolean equals(user other) {
+        return this.getUsername().equals(other.getUsername());
+    }
+    public boolean Password_Match(String password)
+    {
+        return this.getPassword().equals(password);
+    }
+}
