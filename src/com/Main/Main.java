02bf307665ea93754cda3ed45cf69928ad93674e
Kiri <1936684518@qq.com>
2024-06-08 12:08:45 +0800

    done

--- src/com/Main/Main.java
+++ src/com/Main/Main.java
@@ -1,15 +1,22 @@
 package com.Main;
 
 import com.UIS.LoginUI;
+import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
         LoginUI loginUI = new LoginUI();
         try {
             loginUI.start(new Stage());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
